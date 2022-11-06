c7542aa4f587fe02afdc8aa278da9bc4b5ccd9bb
Anandha Narayanan <thecoffeedeveloper@gmail.com>
2022-11-06 17:45:37 +0000

    user async await changes

--- routes/users.route.js
+++ routes/users.route.js
@@ -3,10 +3,10 @@ const { createUser, userLogin } = require('../controllers/users.controller');
 var router = express.Router();
 
 
-router.post('/register', (req, res, next) => {
+router.post('/register', async (req, res, next) => {
   try {
     if (req.body.username && req.body.password && req.body.type ){
-      const user = createUser(req.body.username, req.body.password, req.body.type)
+      const user = await createUser(req.body.username, req.body.password, req.body.type)
       res.status(200).send({status: 'success', message: 'user created successfully.', data: user})
     }else{
       res.status(200).send({status: 'failure', message: 'Please provide required fields.'})
@@ -17,10 +17,10 @@ router.post('/register', (req, res, next) => {
   }
 })
 
-router.get('/login/:username/:password', function(req, res, next) {
+router.get('/login/:username/:password', async(req, res, next) => {
   try {
     if (req.params.username && req.params.password ){
-      const authenticated = userLogin(req.params.username, req.params.password)
+      const authenticated = await userLogin(req.params.username, req.params.password)
       if (authenticated){
         res.status(200).send({status: 'success', message: 'user login successfully.'})
         

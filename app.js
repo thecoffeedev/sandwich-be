10e29a6b436b012b1b281dd4ed758658b4b52ad1
Anandha <balua2@uni.coventry.ac.uk>
2022-12-05 06:00:03 +0000

    added router to the main file

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users.route');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users.route'));
+app.use('/products', require('./routes/products.route'))
+app.use('/orders', require('./routes/orders.route'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
